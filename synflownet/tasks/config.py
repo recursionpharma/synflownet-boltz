c437bb6a8f53cd4ebacc8d867d273e3327c475e2
Kyle Throssell <kthrossell@montai.com>
2025-07-22 19:54:15 +0000

    added building blocks data and updated the config to refer to these

--- synflownet/tasks/config.py
+++ synflownet/tasks/config.py
@@ -12,8 +12,8 @@ class SEHTaskConfig(StrictDataClass):
 @dataclass
 class ReactionTaskConfig(StrictDataClass):
     templates_filename: str = "templates.txt"
-    building_blocks_filename: str = "building_blocks.txt"
-    precomputed_bb_masks_filename: str = "precomputed_bb_masks.pkl"
+    building_blocks_filename: str = "pharmaron_bbs.txt
+    precomputed_bb_masks_filename: str = "precomputed_bb_masks_pharmaron_bbs.pkl" 
     reverse_templates_filename: Optional[str] = None
     reward: Optional[str] = None
     building_blocks_costs: Optional[str] = None
